264b791be02d78ae13473f8148a2b60ee77e1f5f
Julian Minder <julian.minder@icloud.com>
2021-05-11 13:46:46 +0200

    updated functionality of eps-greedy, beautified optimizing function to a more cleaner look

--- src/dqn.py
+++ src/dqn.py
@@ -9,7 +9,7 @@ import math
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-Transition = namedtuple("Transition", "obs action next_obs reward")
+Transition = namedtuple("Transition", "obs action next_obs reward done")
 
 class ReplayMemory:
     def __init__(self, capacity):
@@ -18,11 +18,9 @@ class ReplayMemory:
     def __len__(self):
         return len(self.memory)
 
-    def push(self, obs, action, next_obs, reward):
-        action = torch.tensor([[action]], device=device)
-        reward = torch.tensor([reward], device=device)
-        #done = torch.tensor(done, device=device)
-        self.memory.append(Transition(obs, action, next_obs, reward))
+    def push(self, obs, action, next_obs, reward, done):
+        """Add elements to memory"""
+        self.memory.append(Transition(obs.squeeze(), action, next_obs.squeeze(), reward, done))
 
     def sample(self, batch_size):
         """
@@ -61,31 +59,37 @@ class DQN(nn.Module):
 
         return x
 
+    def load_state(self, other):
+        self.load_state_dict(other.state_dict())
+
     def get_epsilon(self):
-        self.steps_done += 1
         if self.anneal_type == 'exp':
             return self.eps_end + (self.eps_start  -  self.eps_end) * \
                         math.exp(-1. * self.steps_done /  self.anneal_length)
         elif self.anneal_type == 'lin':
-            if self.steps_done > self.anneal_length:
+            if self.steps_done >= self.anneal_length:
                 return self.eps_end
             else:
                 return self.eps_start - (self.eps_start - self.eps_end) * (self.steps_done / self.anneal_length)
         else:
-            raise NotImplmentedError
+            raise NotImplementedError
 
     def act(self, observation, exploit=False):
         """Selects an action with an epsilon-greedy exploration strategy."""
+        # Compute exploiting actions
+        with torch.no_grad():
+            q_values = self.forward(observation)
+        actions = torch.argmax(q_values, dim=1)
+
+        # Add exploration
+        # only non exploiting calls will anneal epsilon
         if not exploit:
-            # only non exploiting calls will anneal epsilon
             eps = self.get_epsilon()
-        else:
-            eps = 0
-        if random.random() > eps or exploit:
-            with torch.no_grad():
-                return self(observation).max(1)[1].view(1,1)
-        else:
-            return torch.tensor([[random.randrange(self.n_actions)]], device=device, dtype=torch.long)
+            roll = torch.rand(observation.shape[0])
+            exploration_indices = roll < eps
+            actions[exploration_indices] = torch.randint(high=self.n_actions, size=[sum(exploration_indices)])
+            self.steps_done += observation.shape[0]
+        return actions
 
 def optimize(dqn, target_dqn, memory, optimizer, grad_clip = False):
     """This function samples a batch from the replay buffer and optimizes the Q-network."""
@@ -96,26 +100,21 @@ def optimize(dqn, target_dqn, memory, optimizer, grad_clip = False):
     # Sample batch
     batch = memory.sample(dqn.batch_size)
 
-    obs_batch = torch.cat(batch.obs).to(device)
-    actions_batch = torch.cat(batch.action).to(device)
-    non_final_next_obs_batch = torch.cat([obs for obs in batch.next_obs if obs is not None]).to(device)
-    rewards_batch = torch.cat(batch.reward).to(device)
+    obs = torch.stack(batch.obs).to(device)
+    actions = torch.tensor(batch.action, device=device)
+    next_obs = torch.stack(batch.next_obs).to(device)
+    reward = torch.tensor(batch.reward, device=device)
+    terminal = torch.tensor(batch.done, device=device)
 
-    # create Terminated Mask
-    term_mask = torch.tensor(tuple(map(lambda obs: obs is not None, batch.next_obs)))
+    all = dqn.forward(obs)
+    actions_b = actions.unsqueeze(1)
+    q_values = all.gather(1, actions_b).squeeze()
 
-    # Calculate q values
-    q_values = dqn(obs_batch).gather(1, actions_batch)
-
-    # Calculate q value targets
-    non_final_qt_max = target_dqn(non_final_next_obs_batch).max(1)[0].detach()
-
-    q_values_targets = torch.zeros(dqn.batch_size, device=device)
-    q_values_targets[term_mask] = non_final_qt_max
-    q_values_targets = (dqn.gamma * q_values_targets) + rewards_batch
+    max_next_q_values = target_dqn.forward(next_obs).max(1)[0]
+    q_value_targets = reward + ~terminal * target_dqn.gamma * max_next_q_values
 
     # Compute loss.
-    loss = F.mse_loss(q_values, q_values_targets.unsqueeze(1))
+    loss = F.mse_loss(q_values, q_value_targets)
 
     # Perform gradient descent.
     # Zeros gradients before running backwards pass
@@ -129,4 +128,5 @@ def optimize(dqn, target_dqn, memory, optimizer, grad_clip = False):
             param.grad.data.clamp_(-1, 1)
 
     optimizer.step()
+
     return loss.item()
